5a3c10e733a561e8bdb5ab8f8542039f980dca62
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-09-27 21:34:28 +0200

    added Comments schema

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE posts (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
 CREATE INDEX posts_created_at_index ON posts (created_at);
+CREATE TABLE comments (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    post_id UUID NOT NULL,
+    author TEXT NOT NULL,
+    body TEXT NOT NULL
+);
+CREATE INDEX comments_created_at_index ON comments (created_at);
+CREATE INDEX comments_post_id_index ON comments (post_id);
+ALTER TABLE comments ADD CONSTRAINT comments_ref_post_id FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE;
