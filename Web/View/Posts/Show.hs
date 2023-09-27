7ba9a0d87caba41c3a5bf9692c476c70ce89ec38
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-09-27 20:00:13 +0200

    Displaying a post

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -1,13 +1,13 @@
 module Web.View.Posts.Show where
 import Web.View.Prelude
 
-data ShowView = ShowView { post :: Post }
+newtype ShowView = ShowView { post :: Post }
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
         {breadcrumb}
-        <h1>Show Post</h1>
-        <p>{post}</p>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
 
     |]
         where
